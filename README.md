996820554743d06486b88e35e2cb0c6ac0dbc54d
Pedro Magno <pedromagnopro@gmail.com>
2024-01-19 00:50:17 -0300

    Instrumenting

---
+++ README.md
@@ -0,0 +1,3 @@
+# C# 10 and .NET 6 Modern Cross-Platform Development Projects
+
+Este diretório contém uma série de projetos de estudo relacionados ao livro "C# 10 and .NET 6 Modern Cross-Platform Development". Os projetos foram criados com o objetivo de auxiliar no aprendizado e na aplicação prática dos conceitos apresentados no livro.
---
+++ chapter04/Instrumenting/Instrumenting/Instrumenting/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+
+//write to a text file in the project folder
+Trace.Listeners.Add(new TextWriterTraceListener(
+    File.CreateText(Path.Combine(Environment.GetFolderPath(
+        Environment.SpecialFolder.DesktopDirectory), "log.txt"))));
+
+//Text writer is buffered, so this options calls
+//Flush() on all listeners after writing
+Trace.AutoFlush = true;
+
+Debug.WriteLine("Debug says, I am watching!");
+Trace.WriteLine("Trace says, I am watching!");
+
+ConfigurationBuilder builder = new();
+
+builder.SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+IConfiguration configuration = builder.Build();
+
+TraceSwitch ts = new(displayName: "PacktSwitch", description: "This switch is set via a JSON config.");
+
+configuration.GetSection("PacktSwitch").Bind(ts);
+
+Trace.WriteLineIf(ts.TraceError, "Trace error");
+Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
